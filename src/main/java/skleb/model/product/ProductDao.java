733ccb555f521efecc66bd77832404c4d3df85ba
Piotr Przybylak <piotr.przybylak@gmail.com>
2020-10-15 16:46:32 +0200

    java version for heroku

--- src/main/java/skleb/model/product/ProductDao.java
+++ src/main/java/skleb/model/product/ProductDao.java
@@ -1,11 +1,12 @@
 package skleb.model.product;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 
 public class ProductDao {
     public List<Product> all() {
-        return List.of(
+        return Arrays.asList(
                 new Product("Mydło", "", new BigDecimal("25.00")),
                 new Product("Masło", "", new BigDecimal("99.99")),
                 new Product("Chleb", "", new BigDecimal("5.50")));
